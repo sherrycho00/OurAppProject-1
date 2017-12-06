34d6c794b38ba23d0ae186bdf195593058cfe746
sherry <qixiahou@gmail.com>
2017-12-06 12:58:48 +0800

    1

--- app/src/main/java/com/example/elsie/framelayout/Setting/PersonalModel.java
+++ app/src/main/java/com/example/elsie/framelayout/Setting/PersonalModel.java
@@ -1,12 +1,24 @@
 package com.example.elsie.framelayout.Setting;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by sherry on 17-12-1.
  * 存储用户个人信息
  */
 
 public class PersonalModel{
-    public static String name;
-    public static String mail;
-    private String userpath;
+    private long id;
+    private String name;
+    private String mail;
+    private Bitmap photo;
+
+    long getId(){return id;}
+    String getName(){return name;}
+    String getMail(){return mail;}
+    Bitmap getPhoto(){return photo;}
+    void setId(long id){this.id=id;}
+    void setName(String name){this.name=name;}
+    void setMail(String mail){this.mail=mail;}
+    void setPhoto(Bitmap photo){this.photo=photo;}
 }
